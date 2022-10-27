8c95b7f9203ba8d4184802e9a38697ecc917bfff
Kamelya Kerum <Kamelya.kerum@gmail.com>
2022-10-27 06:23:21 +0100

    m

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,37 +1,42 @@
 class BookmarksController < ApplicationController
-    before_action :set_bookmark, only: :destroy
-    before_action :set_list, only: [:new, :create]
+
+  class ListsController < ApplicationController
+    before_action :set_list, only: [:show, :destroy]
+
+    def index
+      @lists = List.all
+    end
+
+    def show
+      @bookmark = Bookmark.new
+      @review = Review.new(list: @list)
+    end
 
     def new
-      @bookmark = Bookmark.new
+      @list = List.new
     end
 
     def create
-      @bookmark = Bookmark.new(bookmark_params)
-      @bookmark.list = @list
-      if @bookmark.save
+      @list = List.new(list_params)
+      if @list.save
         redirect_to list_path(@list)
       else
-        render :new
+        render :new, status: :unprocessable_entity
       end
     end
 
     def destroy
-      @bookmark.destroy
-      redirect_to list_path(@bookmark.list), status: :see_other
+      @list.destroy
+      redirect_to lists_path, status: :see_other
     end
 
     private
 
-    def bookmark_params
-      params.require(:bookmark).permit(:comment, :movie_id)
-    end
-
-    def set_bookmark
-      @bookmark = Bookmark.find(params[:id])
-    end
-
     def set_list
-      @list = List.find(params[:list_id])
+      @list = List.find(params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name, :photo)
     end
   end
